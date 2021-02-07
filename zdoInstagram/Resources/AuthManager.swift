bf7666a73b6b5a045ac437a21bcae7a995a8fbc7
zdodev <iver0945@gmail.com>
2021-02-07 12:26:03 +0900

    ✨: Add auth manager.

---
+++ zdoInstagram/Resources/AuthManager.swift
@@ -0,0 +1,14 @@
+import FirebaseAuth
+
+class AuthManager {
+    static let shared = AuthManager()
+    
+    // Mark: - Public
+    func registerNewUser(username: String, email: String, password: String) {
+        
+    }
+    
+    func loginUser(username: String?, email: String?, password: String) {
+        
+    }
+}
