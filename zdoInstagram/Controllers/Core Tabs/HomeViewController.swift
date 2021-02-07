ef547213e793c55d310aafc039dbaf249ec4eeff
zdodev <iver0945@gmail.com>
2021-02-07 12:21:19 +0900

    ✨: Handling when user login is not authenticated.

--- zdoInstagram/Controllers/Core Tabs/HomeViewController.swift
+++ zdoInstagram/Controllers/Core Tabs/HomeViewController.swift
@@ -1,4 +1,20 @@
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        handleNotAuthenticated()
+    }
+    
+    private func handleNotAuthenticated() {
+        // Check auth status
+        if Auth.auth().currentUser == nil {
+            // Show log in
+            let loginVC = LoginViewController()
+            loginVC.modalPresentationStyle = .fullScreen
+            present(loginVC, animated: true)
+        }
+    }
 }
