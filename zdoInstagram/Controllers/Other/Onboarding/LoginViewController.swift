6381ccf923fbde91708c9963718fde9d818ff285
zdodev <iver0945@gmail.com>
2021-02-07 12:24:04 +0900

    🎨: Define UI components.

--- zdoInstagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ zdoInstagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -1,4 +1,60 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    private let usernameEmailField: UITextField = {
+        UITextField()
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        UIButton()
+    }()
+    
+    private let termsButton: UIButton = {
+        UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+        UIButton()
+    }()
+    
+    private let createAccountButton: UIButton = {
+        UIButton()
+    }()
+    
+    private let headerView: UIView = {
+        UIView()
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubViews()
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // assign frames
+    }
+    
+    private func addSubViews() {
+        view.addSubview(usernameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(createAccountButton)
+        view.addSubview(headerView)
+    }
+    
+    @objc private func didTapLoginButton() {}
+    @objc private func didTapTermsButton() {}
+    @objc private func didTapPrivacyButton() {}
+    @objc private func didTapCreateAccountButton() {}
 }
