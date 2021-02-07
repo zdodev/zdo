e4a3941dad3ae1cd73fd130300d6c57be035f1cc
zdodev <iver0945@gmail.com>
2021-02-07 12:25:21 +0900

    ✨: Add firebaseApp configure.

--- zdoInstagram/Resources/AppDelegate.swift
+++ zdoInstagram/Resources/AppDelegate.swift
@@ -1,9 +1,11 @@
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        true
+        FirebaseApp.configure()
+        return true
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
