c4821ee9492fdc6aec60a591b8a78c7b69483cbb
zdodev <iver0945@gmail.com>
2021-02-07 17:42:48 +0900

    ✨: Add size and position computed properties to UIView.

--- zdoInstagram/Resources/Extensions.swift
+++ zdoInstagram/Resources/Extensions.swift
@@ -1,8 +1,27 @@
-//
-//  Extensions.swift
-//  zdoInstagram
-//
-//  Created by Zero DotOne on 2021/02/07.
-//
+import UIKit
 
-import Foundation
+extension UIView {
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+    
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        frame.origin.y + frame.size.height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        frame.origin.x + frame.size.width
+    }
+}
