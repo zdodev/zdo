95bf5d8f71ddc12704f07b074133f721240c89d3
zdodev <iver0945@gmail.com>
2021-02-08 23:11:05 +0900

    ✨: Implement user registration function.

--- zdoInstagram/Resources/AuthManager.swift
+++ zdoInstagram/Resources/AuthManager.swift
@@ -4,11 +4,58 @@ class AuthManager {
     static let shared = AuthManager()
     
     // Mark: - Public
-    func registerNewUser(username: String, email: String, password: String) {
-        
+    func registerNewUser(username: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        /*
+         - Check if username is available
+         - Check if email is available
+         - Create account
+         - Insert account to database
+         */
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { canCreate in
+            if canCreate {
+                /*
+                 - Create account
+                 - Insert account to database
+                 */
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    guard error == nil, result != nil else {
+                        // Firebase auth count not create account
+                        completion(false)
+                        return
+                    }
+                    
+                    // Inssert into database
+                    DatabaseManager.shared.insertNewUser(with: email, username: username) { inserted in
+                        if inserted {
+                            completion(true)
+                            return
+                        } else {
+                            // Failed to inser to database
+                            completion(false)
+                            return
+                        }
+                    }
+                }
+            } else {
+                // either username or email does not exist
+                completion(false)
+            }
+        }
     }
     
-    func loginUser(username: String?, email: String?, password: String) {
-        
+    func loginUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void) {
+        if let email = email {
+            // email log in
+            Auth.auth().signIn(withEmail: email, password: password) {  authResult, error in
+                guard authResult != nil, error == nil else {
+                    completion(false)
+                    return
+                }
+                completion(true)
+            }
+        } else if let username = username {
+            // username log in
+            print(username)
+        }
     }
 }
