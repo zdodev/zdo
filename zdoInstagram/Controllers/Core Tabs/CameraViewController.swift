c5e3b6f291dd2c270e428e091a5c99da4e0aace8
zdodev <iver0945@gmail.com>
2021-02-09 01:25:08 +0900

    🤡: Mock UI.

--- zdoInstagram/Controllers/Core Tabs/CameraViewController.swift
+++ zdoInstagram/Controllers/Core Tabs/CameraViewController.swift
@@ -1,4 +1,14 @@
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    private func didTapTakePicture() {
+        
+    }
 }
