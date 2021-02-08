9728f6cd1161d566a7d5e2228120e3b6bdcbe44c
zdodev <iver0945@gmail.com>
2021-02-09 01:22:16 +0900

    ✨: Implement the logout in the configuration menu.

--- zdoInstagram/Controllers/Other/General/SettingsViewController.swift
+++ zdoInstagram/Controllers/Other/General/SettingsViewController.swift
@@ -1,4 +1,90 @@
 import UIKit
 
-class SettingsViewController: UIViewController {
+struct SettingCellModel {
+    let title: String
+    let handler: () -> Void
+}
+
+// View Controller to show user settings
+final class SettingsViewController: UIViewController {
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var data = [[SettingCellModel]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureModels()
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    }
+    
+    private func configureModels() {
+        let section = [
+            SettingCellModel(title: "Log Out") { [weak self] in
+                self?.didTapLogOut()
+            }
+        ]
+        data.append(section)
+    }
+    
+    private func didTapLogOut() {
+        let actionSheet = UIAlertController(title: "Log Out", message: "Are you sure you want to log out?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        actionSheet.addAction(UIAlertAction(title: "Log Out", style: .destructive) { _ in
+            AuthManager.shared.logOut { success in
+                DispatchQueue.main.async {
+                    if success {
+                        // present log in
+                        let loginVC = LoginViewController()
+                        loginVC.modalPresentationStyle = .fullScreen
+                        self.present(loginVC, animated: true) {
+                            self.navigationController?.popToRootViewController(animated: true)
+                            self.tabBarController?.selectedIndex = 0
+                        }
+                    } else {
+                        // error occurred
+                        fatalError("Could not log out user")
+                    }
+                }
+            }
+        })
+        actionSheet.popoverPresentationController?.sourceView = tableView
+        actionSheet.popoverPresentationController?.sourceRect = tableView.bounds
+        present(actionSheet, animated: true)
+    }
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.section][indexPath.row].title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = data[indexPath.section][indexPath.row]
+        model.handler()
+    }
 }
