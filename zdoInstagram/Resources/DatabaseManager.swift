62c4ed4d0f5f05e495501d4e9ba13ae36cb4f3cd
zdodev <iver0945@gmail.com>
2021-02-08 23:10:04 +0900

    ✨: Implement a function to store user information in a database.

--- zdoInstagram/Resources/DatabaseManager.swift
+++ zdoInstagram/Resources/DatabaseManager.swift
@@ -2,4 +2,33 @@ import FirebaseDatabase
 
 class DatabaseManager {
     static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+    
+    /// Check if username and email is available
+    /// - Parameters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        completion(true)
+    }
+    
+    /// Inserts new user data to database
+    /// - Parameters
+    ///     - email: String representing email
+    ///     - username: String representing username
+    ///     - completion: Async callback for result if database entry succeeded
+    func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        database.child(email.safeDatabaseKey()).setValue(["username": username]) { error, _ in
+            if error == nil {
+                // successed
+                completion(true)
+                return
+            } else {
+                // failed   
+                completion(false)
+                return
+            }
+        }
+    }
 }
