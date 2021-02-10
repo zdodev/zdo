6d71a818576a9e1b5b62d8073948f84099ed4460
zdodev <iver0945@gmail.com>
2021-02-10 22:03:10 +0900

    Make up the homeViewController.

--- zdoInstagram/Controllers/Core Tabs/HomeViewController.swift
+++ zdoInstagram/Controllers/Core Tabs/HomeViewController.swift
@@ -2,10 +2,25 @@ import UIKit
 import FirebaseAuth
 
 class HomeViewController: UIViewController {
+    private let tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.register(IGFeedPostTableViewCell.self, forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
+        return tableView
+    }()
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         
         handleNotAuthenticated()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
     }
     
     private func handleNotAuthenticated() {
@@ -17,5 +32,20 @@ class HomeViewController: UIViewController {
             loginVC.modalPresentationStyle = .fullScreen
             present(loginVC, animated: true)
         }
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
+        return cell
     }
 }
