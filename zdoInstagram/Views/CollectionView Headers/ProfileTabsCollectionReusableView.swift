603fe0b7f88fa89098431c80a492d0654ff2e172
zdodev <iver0945@gmail.com>
2021-02-11 00:12:56 +0900

    🚧: Work in progress.

--- zdoInstagram/Views/CollectionView Headers/ProfileTabsCollectionReusableView.swift
+++ zdoInstagram/Views/CollectionView Headers/ProfileTabsCollectionReusableView.swift
@@ -1,12 +1,5 @@
-//
-//  ProfileTabsCollectionReusableView.swift
-//  zdoInstagram
-//
-//  Created by Zero DotOne on 2021/02/10.
-//
-
 import UIKit
 
 class ProfileTabsCollectionReusableView: UICollectionReusableView {
-        
+    static let identifier = "ProfileTabsCollectionReusableView"
 }
