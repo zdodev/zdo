12f968063ecce0a781cf28db24a281becc6b3516
zdodev <iver0945@gmail.com>
2021-02-11 00:12:25 +0900

    🚧: Work in progress.

--- zdoInstagram/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
+++ zdoInstagram/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -1,12 +1,5 @@
-//
-//  ProfileInfoHeaderCollectionReusableView.swift
-//  zdoInstagram
-//
-//  Created by Zero DotOne on 2021/02/10.
-//
-
 import UIKit
 
 class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
-        
+    static let identifier = "ProfileInfoHeaderCollectionReusableView"
 }
